96e5f8f06d8603fd359a2b186c58111dee54ed85
kumesh1997 <tharindukumesh09@gmail.com>
2023-12-07 09:25:16 +0530

    home page changed

--- application/views/frontend/default/home.php
+++ application/views/frontend/default/home.php
@@ -2,57 +2,48 @@
     <div class="container-lg">
         <div class="row">
             <div class="col">
+                <!-- Content -->
                 <div class="home-banner-wrap">
                     <h2><?php echo get_frontend_settings('banner_title'); ?></h2>
                     <p><?php echo get_frontend_settings('banner_sub_title'); ?></p>
-                    <form class="" action="<?php echo site_url('home/search'); ?>" method="get">
-                        <div class="input-group">
-                            <input type="text" class="form-control" name = "query" placeholder="<?php echo get_phrase('what_do_you_want_to_learn'); ?>?">
-                            <div class="input-group-append">
-                                <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+                </div>
+                <div class="container-lg home-fact">
+                    <div class="row">
+                    <?php $courses = $this->crud_model->get_courses(); ?>
+                    <div class="col-md-3 d-flex">
+                        <div class="home-fact-box mr-md-auto ml-auto mr-auto">
+                            <i class="fas fa-bullseye float-left"></i>
+                            <div class="text-box">
+                                <h4><?php
+                                $status_wise_courses = $this->crud_model->get_status_wise_courses();
+                                $number_of_courses = $status_wise_courses['active']->num_rows();
+                                echo $number_of_courses.' '.get_phrase('online_courses'); ?></h4>
+                                <p><?php echo get_phrase('explore_a_variety_of_fresh_topics'); ?></p>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
+                    </div>
+
+                    <div class="col-md-3 d-flex">
+                        <div class="home-fact-box mr-md-auto ml-auto mr-auto">
+                            <i class="fa fa-check float-left"></i>
+                            <div class="text-box">
+                                <h4><?php echo get_phrase('expert_instruction'); ?></h4>
+                                <p><?php echo get_phrase('find_the_right_course_for_you'); ?></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 d-flex">
+                        <div class="home-fact-box mr-md-auto ml-auto mr-auto">
+                            <i class="fa fa-clock float-left"></i>
+                            <div class="text-box">
+                                <h4><?php echo get_phrase('lifetime_access'); ?></h4>
+                                <p><?php echo get_phrase('learn_on_your_schedule'); ?></p>
+                            </div>
+                        </div>
+                    </div>
         </div>
     </div>
-</section>
-<section class="home-fact-area">
-    <div class="container-lg">
-        <div class="row">
-            <?php $courses = $this->crud_model->get_courses(); ?>
-            <div class="col-md-4 d-flex">
-                <div class="home-fact-box mr-md-auto ml-auto mr-auto">
-                    <i class="fas fa-bullseye float-left"></i>
-                    <div class="text-box">
-                        <h4><?php
-                        $status_wise_courses = $this->crud_model->get_status_wise_courses();
-                        $number_of_courses = $status_wise_courses['active']->num_rows();
-                        echo $number_of_courses.' '.get_phrase('online_courses'); ?></h4>
-                        <p><?php echo get_phrase('explore_a_variety_of_fresh_topics'); ?></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 d-flex">
-                <div class="home-fact-box mr-md-auto ml-auto mr-auto">
-                    <i class="fa fa-check float-left"></i>
-                    <div class="text-box">
-                        <h4><?php echo get_phrase('expert_instruction'); ?></h4>
-                        <p><?php echo get_phrase('find_the_right_course_for_you'); ?></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 d-flex">
-                <div class="home-fact-box mr-md-auto ml-auto mr-auto">
-                    <i class="fa fa-clock float-left"></i>
-                    <div class="text-box">
-                        <h4><?php echo get_phrase('lifetime_access'); ?></h4>
-                        <p><?php echo get_phrase('learn_on_your_schedule'); ?></p>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
