1dca4d9d0d8812d84fb9d4c2a94af44843fb55d5
kumesh1997 <tharindukumesh09@gmail.com>
2023-12-07 22:39:54 +0530

    updated

--- application/views/frontend/default/home.php
+++ application/views/frontend/default/home.php
@@ -1,11 +1,69 @@
+<script>
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+</script>
 <section class="home-banner-area">
     <div class="container-lg">
         <div class="row">
             <div class="col">
                 <!-- Content -->
                 <div class="home-banner-wrap">
-                    <h2 class="line-1 anim-typewriter"><?php echo get_frontend_settings('banner_title'); ?></h2>
-                    <p><?php echo get_frontend_settings('banner_sub_title'); ?></p>
+                    <h2 class="typewrite" data-period="2000" data-type='[ "<?php echo get_frontend_settings('banner_title'); ?>...", "Learn Anytime, Anywhere", "Smart Learning, Your Way"]'></h2>
+                    <p style="color: yellow"><?php echo get_frontend_settings('banner_sub_title'); ?></p>
                 </div>
                 <div class="container-lg home-fact">
                     <div class="row">
